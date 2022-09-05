18a2fde8928dcd6f596a23822d2df5b25c7ed9ac
Dirley f <dirleyoliveirabrasil@gmail.com>
2022-09-05 19:27:18 -0300

    fist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
-
+import NoticationButton from "./componentes/notificationButton/aindex"
 
 function App() {
-  return(<h1>Óla mundo!</h1>)
+  return(
+    <>
+     <h1>Olá mundo!</h1>
+     <NoticationButton />
+     </>
+  )
 }
 
 export default App
---
+++ frontend/src/componentes/notificationButton/aindex.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/react.svg'
+
+import './styles.css'
+
+function NoticationButton() {
+    return (
+        <div className="dsmeta-red-btn" >
+            <img src={icon} alt="Notificar" />
+        </div >
+    )
+}
+
+export default NoticationButton
